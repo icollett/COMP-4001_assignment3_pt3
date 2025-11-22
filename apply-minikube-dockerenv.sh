575104edf886a62411eea7350629a87418cea270
icollett <icollett@academic.rrc.ca>
2025-11-22 14:56:28 -0600

    Label removal in config-map for nginx.

--- apply-minikube-dockerenv.sh
+++ apply-minikube-dockerenv.sh
@@ -6,8 +6,9 @@ DOCKER_IMAGE_TAG=latest  # You can change the image tag here if needed
 K8S_DIR="./k8s"  # Path to the directory containing your Kubernetes manifests
 
 # Force Docker to use Minikube's Docker daemon
+echo "Setting Docker to minikube docker daemon"
 eval $(minikube docker-env)
-sleep 3s
+sleep 2s
 
 # Build Docker images in Minikube's Docker daemon
 echo "Building Docker images inside Minikube's Docker daemon..."
@@ -25,13 +26,10 @@ kubectl apply -f $K8S_DIR  # Apply all YAML files in the 'k8s' directory
 
 # Restart deployments to pick up the newly built images
 echo "Restarting deployments to pick up the newly built images..."
-kubectl rollout restart deployment/backend
-kubectl rollout restart deployment/transactions
-kubectl rollout restart deployment/studentportfolio
-kubectl rollout restart deployment/nginx
+kubectl rollout restart deployment.apps
 
 # Wait for pods to be created
 echo "Waiting for the pods to be created..."
-kubectl get pods
+kubectl get pods --wait
 
 minikube service nginx
